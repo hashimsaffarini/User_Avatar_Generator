37f16b8880209c505625d2f34eb60d1326a0789e
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-08-30 18:56:58 +0300

    Add initial project files and structure

---
+++ lib/user_avatar_generator.dart
@@ -0,0 +1,12 @@
+library user_avatar_generator;
+
+import 'package:flutter/widgets.dart';
+
+class UserAvatarGenerator extends StatelessWidget {
+  const UserAvatarGenerator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ test/user_avatar_generator_test.dart
@@ -0,0 +1,10 @@
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:user_avatar_generator/user_avatar_generator.dart';
+
+void main() {
+  // test('Generate user avatar', () {
+  //   final userAvatar = UserAvatarGenerator.generate('John Doe');
+  //   expect(userAvatar, isNotNull);
+  // });
+
+}
