0423c7a51a23d61bf8e561ea98ca971fe861c231
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-08-30 19:53:25 +0300

    chore: Update pubspec.yaml and user_avatar_generator.dart
    The changes include updating the `pubspec.yaml` file to add the `user_avatar_generator` package as a dependency and updating the `user_avatar_generator.dart` file to add new features and improve code structure.

--- lib/user_avatar_generator.dart
+++ lib/user_avatar_generator.dart
@@ -1,12 +1,94 @@
 library user_avatar_generator;
 
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class UserAvatarGenerator extends StatelessWidget {
-  const UserAvatarGenerator({super.key});
+  final String? generatedText; // The text to be generated (e.g., initials)
+  final String? readyText; // Predefined text that can be used directly
+  final double? avatarHeight; // Height of the avatar widget
+  final double? avatarWidth; // Width of the avatar widget
+  final Color backgroundColor; // Background color of the avatar
+  final Color textColor; // Color of the text inside the avatar
+  final TextStyle? textStyle; // Custom text style for the avatar text
+  final BoxShape avatarShape; // Shape of the avatar (e.g., circle or rectangle)
+  final BoxBorder? border; // Border for the avatar
+  final double? textSize; // Size of the text inside the avatar
+  final Alignment textAlignment; // Alignment of the text within the avatar
+  final bool
+      isUpperCase; // Whether the initials should be uppercase or lowercase
+  final int
+      numberOfCharacters; // Number of characters to include in the initials
+
+  const UserAvatarGenerator({
+    super.key,
+    this.generatedText,
+    this.readyText,
+    this.avatarHeight,
+    this.avatarWidth,
+    this.backgroundColor = Colors.blue,
+    this.textColor = Colors.white,
+    this.textStyle,
+    this.avatarShape = BoxShape.circle,
+    this.border, // Simplified border property
+    this.textSize,
+    this.textAlignment = Alignment.center,
+    this.isUpperCase = true, // Default to uppercase
+    this.numberOfCharacters = 2, // Default to 2 characters for initials
+  });
+
+  String generateInitials(String fullName,
+      {bool isUpperCase = true, int numberOfCharacters = 2}) {
+    List<String> names = fullName
+        .trim()
+        .split(RegExp(r'\s+')); // Split by any amount of whitespace
+    String initials = names.map((name) => name[0]).join();
+
+    if (initials.length > numberOfCharacters) {
+      initials = initials.substring(0, numberOfCharacters);
+    }
+
+    return isUpperCase ? initials.toUpperCase() : initials.toLowerCase();
+  }
+
+  String _getText() {
+    if (readyText != null) {
+      return readyText!;
+    } else if (generatedText != null) {
+      return generateInitials(generatedText!,
+          isUpperCase: isUpperCase, numberOfCharacters: numberOfCharacters);
+    } else {
+      return '';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    final double calculatedAvatarHeight = avatarHeight ?? screenHeight * 0.1;
+    final double calculatedAvatarWidth = avatarWidth ?? screenWidth * 0.2;
+    final double calculatedTextSize = textSize ?? calculatedAvatarHeight * 0.4;
+
+    return Container(
+      width: calculatedAvatarWidth,
+      height: calculatedAvatarHeight,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        shape: avatarShape,
+        border: border, // Use the simplified border property
+      ),
+      child: Align(
+        alignment: textAlignment,
+        child: Text(
+          _getText(),
+          style: textStyle ??
+              TextStyle(
+                color: textColor,
+                fontSize: calculatedTextSize,
+              ),
+        ),
+      ),
+    );
   }
 }
