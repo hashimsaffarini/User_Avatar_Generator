9837e6e8f2bf6c83285dbdef4c71fbf8d82b351f
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-08-31 12:21:46 +0300

    custom edits im widget

---
+++ lib/models/backgrounds_model.dart
@@ -0,0 +1,7 @@
+class AvatarBackgrounds {
+  static const String background1 = 'assets/svgs/background1.svg';
+
+  static const List<String> allBackgrounds = [
+    background1,
+  ];
+}
--- lib/user_avatar_generator.dart
+++ lib/user_avatar_generator.dart
@@ -2,12 +2,20 @@ library user_avatar_generator;
 
 import 'package:flutter/material.dart';
 
+enum AvatarBackgroundStyle {
+  style1,
+  style2,
+  style3,
+}
+
 class UserAvatarGenerator extends StatelessWidget {
   final String? generatedText; // The text to be generated (e.g., initials)
   final String? readyText; // Predefined text that can be used directly
   final double? avatarHeight; // Height of the avatar widget
   final double? avatarWidth; // Width of the avatar widget
-  final Color backgroundColor; // Background color of the avatar
+  final AvatarBackgroundStyle? backgroundStyle; // Enum for background style
+  final Color? backgroundColor; // Background color of the avatar
+  final String? backgroundImagePath; // Path to the background image
   final Color textColor; // Color of the text inside the avatar
   final TextStyle? textStyle; // Custom text style for the avatar text
   final BoxShape avatarShape; // Shape of the avatar (e.g., circle or rectangle)
@@ -25,22 +33,23 @@ class UserAvatarGenerator extends StatelessWidget {
     this.readyText,
     this.avatarHeight,
     this.avatarWidth,
+    this.backgroundStyle, // Selected background style
     this.backgroundColor = Colors.blue,
+    this.backgroundImagePath,
     this.textColor = Colors.white,
     this.textStyle,
     this.avatarShape = BoxShape.circle,
-    this.border, // Simplified border property
+    this.border,
     this.textSize,
     this.textAlignment = Alignment.center,
-    this.isUpperCase = true, // Default to uppercase
-    this.numberOfCharacters = 2, // Default to 2 characters for initials
+    this.isUpperCase = true,
+    this.numberOfCharacters = 2,
   });
 
+  // Function to generate initials from the full name
   String generateInitials(String fullName,
       {bool isUpperCase = true, int numberOfCharacters = 2}) {
-    List<String> names = fullName
-        .trim()
-        .split(RegExp(r'\s+')); // Split by any amount of whitespace
+    List<String> names = fullName.trim().split(RegExp(r'\s+'));
     String initials = names.map((name) => name[0]).join();
 
     if (initials.length > numberOfCharacters) {
@@ -50,6 +59,20 @@ class UserAvatarGenerator extends StatelessWidget {
     return isUpperCase ? initials.toUpperCase() : initials.toLowerCase();
   }
 
+  // Function to determine the background image based on the selected style
+  String? _getBackgroundImage() {
+    final backgroundImages = {
+      AvatarBackgroundStyle.style1: 'assets/images/back1.jpg',
+      AvatarBackgroundStyle.style2: 'assets/images/back1.jpg',
+      AvatarBackgroundStyle.style3: 'assets/images/back1.jpg',
+    };
+
+    return backgroundStyle != null
+        ? backgroundImages[backgroundStyle]
+        : backgroundImagePath;
+  }
+
+  // Function to get the text to display
   String _getText() {
     if (readyText != null) {
       return readyText!;
@@ -76,7 +99,13 @@ class UserAvatarGenerator extends StatelessWidget {
       decoration: BoxDecoration(
         color: backgroundColor,
         shape: avatarShape,
-        border: border, // Use the simplified border property
+        border: border,
+        image: _getBackgroundImage() != null
+            ? DecorationImage(
+                image: AssetImage(_getBackgroundImage()!),
+                fit: BoxFit.cover,
+              )
+            : null,
       ),
       child: Align(
         alignment: textAlignment,
